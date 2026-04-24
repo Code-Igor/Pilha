cf9c73b3d1b2b7d82021e74d52485ef6ba9c115d
Igor <igordossantoslopes73@gmail.com>
2026-04-24 16:47:49 -0300

    test: realizando testes da funcionalidades, funcionando perfeitamente.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        Pilha pilha = new Pilha(10);
 
+        // esperado: estar vazia
+        pilha.pilhaVazia();
 
+        pilha.empilhar(10); pilha.empilhar(20); pilha.empilhar(30);
+
+        //esperado: não vazia
+        pilha.pilhaVazia();
+
+        // esperado: 30
+        System.out.println(pilha.desempilhar());
+        pilha.desempilhar(); pilha.desempilhar();
+
+        //esperado: vazia
+        pilha.pilhaVazia();
     }
 }
